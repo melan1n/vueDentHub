2252c3b69e7a8fdd69e764943130f1148dded337
Milen Nikolov <milennikolov_bg@yahoo.com>
2020-04-07 23:24:20 +0300

    Moved router.js to plugins folder

--- src/main.js
+++ src/main.js
@@ -1,17 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueRouter from 'vue-router';
-import routes from './routes';
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import router from './plugins/router.js'; //path to router
 
 Vue.config.productionTip = false
-Vue.use(VueRouter);
 
-const router = new VueRouter({
-  mode: 'history',
-  routes
-})
 new Vue({
   render: h => h(App),
   router
---
+++ src/plugins/router.js
@@ -0,0 +1,26 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import AppHome from '@/components/Home.vue';
+import AppRegister from '@/components/shared/Register.vue';
+import AppLogin from '@/components/shared/Login.vue';
+import AppDentist from '@/components/core/Dentist.vue';
+import AppAppointment from '@/components/core/Appointment.vue';
+import AppDetail from '@/components/core/Detail.vue';
+
+const routes = [
+    { path: '', component: AppHome },
+    { path: '/home', component: AppHome },
+    { path: '/register', component: AppRegister },
+    { path: '/login', component: AppLogin },
+    { path: '/logout', component: AppDentist },
+    { path: '/dentist', component: AppDentist },
+    { path: '/appointment', component: AppAppointment },
+    { path: '/detail/:id', component: AppDetail }
+]
+
+Vue.use(VueRouter);
+
+export default new VueRouter({
+  mode: 'history',
+  routes
+})
